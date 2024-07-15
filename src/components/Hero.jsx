bcc2f183b0a5a1a79b96ae675e15668d1e2932c3
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2024-07-16 02:01:50 +0300

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,39 @@
+import React from "react";
+import profile from "../assets/profile.png";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-left">
+        <div className="hero-left-first-div">Ömer Özyılmaz</div>
+
+        <h2>
+          Creative thinker
+          <br />
+          Minimalism lover
+        </h2>
+        <p>
+          Hi, I’m Ömer. I’m a full-stack developer. If you are looking for a
+          Developer who can craft solid and scalable frontend products with
+          great user experiences. Let’s shake hands with me.
+        </p>
+        <div className="hero-buttons">
+          <button className="btn primary-btn">Hire me</button>
+          <button className="btn secondary-btn">
+            <img src={github} alt="" className="scalesf" />
+            <i className="fab fa-github"></i> Github
+          </button>
+          <button className="btn secondary-btn">
+            <img src={linkedin} alt="" className="scalesf" />
+            <i className="fab fa-linkedin"></i> Linkedin
+          </button>
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={profile} alt="Profile" />
+      </div>
+    </div>
+  );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,31 @@
+import React from "react";
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div class="skills-section">
+      <h1 class="skills-title">Skills</h1>
+      <div class="skills-container">
+        <div class="skill">
+          <h2 class="skill-name">Java Script</h2>
+          <p class="skill-description">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="skill">
+          <h2 class="skill-name">React.Js</h2>
+          <p class="skill-description">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="skill">
+          <h2 class="skill-name">Node.Js</h2>
+          <p class="skill-description">
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
